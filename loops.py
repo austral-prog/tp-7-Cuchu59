803ab2af3caa836b2b3fcaa5218e5e2a84f15822
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-05-17 10:23:15 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,30 @@
 def index_of_by_index(word, list, index):
-    return -1
+    last_index = -1
+    if word in list[index:]:
+        last_index = index + list[index:].index(word)
+    return last_index
 
 
 def index_of_empty(list):
+    if "" in list:
+        return list.index("") # es literalmente lo que hace el metodo ".index" 
     return -1
 
 
 def index_of(word, list):
-    return -1
+    index = -1
+    if word in list:
+        index = list.index(word)
+    return index
 
 
 def put(word, list):
-    return -1
-
+    return index_of_empty(list)
+    
 
 def remove(word, list):
-    return -1
+    rems = 0
+    for item in list:
+        if item == word:
+            rems += 1
+    return rems
