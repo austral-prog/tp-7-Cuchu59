2b85eb27625fa31fed02ad4eb44f8d904e82499e
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-05-17 09:26:36 -0400

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,16 @@
 def enumerate_list(list):
-    return list
+    colors = []
+    a = 0
+    for element in list:
+        if element:
+            colors.append(f"{a}. {element}")
+            a += 1 
+    return colors
 
 
 def enumerate_backwards(list):
-    return list
+    new_list = []
+    for element in enumerate_list(list):
+        backw_el = element[:3] + element[-1:-len(element)+2:-1] 
+        new_list.append(backw_el)
+    return new_list
